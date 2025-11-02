0471b14f32c19507b04fbd45a811243cd5a1966e
nehalabbott <nehal27abbott@gmail.com>
2025-11-02 23:33:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -128,13 +128,32 @@ vector<string> parseOperands(const string& line) {
     return tokens;
 }
 
+
 int registerToInt(const string& reg) {
-    if (reg[0] == 'x') {
+    static const unordered_map<string, int> regMap = {
+        {"zero", 0}, {"ra", 1}, {"sp", 2}, {"gp", 3}, {"tp", 4},
+        {"t0", 5}, {"t1", 6}, {"t2", 7},
+        {"s0", 8}, {"fp", 8}, {"s1", 9},
+        {"a0", 10}, {"a1", 11}, {"a2", 12}, {"a3", 13}, {"a4", 14}, {"a5", 15}, {"a6", 16}, {"a7", 17},
+        {"s2", 18}, {"s3", 19}, {"s4", 20}, {"s5", 21}, {"s6", 22}, {"s7", 23}, {"s8", 24}, {"s9", 25},
+        {"s10", 26}, {"s11", 27},
+        {"t3", 28}, {"t4", 29}, {"t5", 30}, {"t6", 31}
+    };
+
+    if (reg.size() > 1 && reg[0] == 'x') {
+        // numeric register like x5
         return stoi(reg.substr(1));
     }
-    // TODO: Add ABI names like "ra", "sp", "a0", etc.
-    return 0; // Error or x0
+
+    auto it = regMap.find(reg);
+    if (it != regMap.end()) {
+        return it->second;
+    }
+
+    // If not found, return 0 (default to x0)
+    return 0;
 }
+
 
 long stringToLong(const string& s) {
     try {
